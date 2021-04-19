58a264c7432ac9fbd01941db4e48ed1db77c7db8
serene1004 <vcbxnzn@naver.com>
2021-04-20 00:26:28 +0900

    update 2021-04-20

--- LITHO_travel/js/litho.js
+++ LITHO_travel/js/litho.js
@@ -62,6 +62,8 @@
             var $slide     = $('#section1 .slide');
             var slideW     = $('#section1 .slide').innerWidth();
             var n          = $('#section1 .slide').length;  // n=3
+            var $prevBtn   = $('#section1 .prev-btn'); 
+            var $nextBtn   = $('#section1 .next-btn'); 
             var cnt        = 0;
             var touchS     = 0;
             var touchE     = 0;
@@ -97,7 +99,7 @@
                 }
 
                 for(var i=0; i<n; i++){
-                    $slide.eq(prev[i]).stop().animate({left:(100*(-i))+'%'}, 0).animate({left:(100*(-i+1))+'%'}, 600, 'easeInOutCubic');
+                    $slide.eq(prev[i]).stop().animate({left:(100*(-i))+'%'}, 0).animate({left:(100*(-i+1))+'%'}, 400, 'easeInOutCubic');
                 }
             }
             function mainNextSlideFn(){
@@ -116,7 +118,7 @@
                  }
 
                 for(var i=0; i<n; i++){
-                    $slide.eq(next[i]).stop().animate({left:(100*i)+'%'}, 0).animate({left:(100*(i-1))+'%'}, 600, 'easeInOutCubic');
+                    $slide.eq(next[i]).stop().animate({left:(100*i)+'%'}, 0).animate({left:(100*(i-1))+'%'}, 400, 'easeInOutCubic');
                 }
             }
 
@@ -130,6 +132,23 @@
                 if(cnt>2){cnt=0;}
                 mainNextSlideFn();
             }
+
+            $prevBtn.on({
+                click:function(){
+                    if(!$slide.is(':animated')){
+                        prevSlideCountFn();
+                    }
+                }
+            });
+            $nextBtn.on({
+                click:function(){
+                    if(!$slide.is(':animated')){
+                        nextSlideCountFn();
+                    }
+                }
+            });
+            
+
 
             $slideView.on({
                 mousedown:function(e){
@@ -153,12 +172,12 @@
             });
 
             function touchSwipeFn(){
-                if(touchS-touchE > 0){
+                if(touchS-touchE > 30){
                     if(!$slide.is(':animated')){
                         nextSlideCountFn();
                     }
                 }
-                if(touchS-touchE < 0){
+                if(touchS-touchE < -30){
                     if(!$slide.is(':animated')){
                         prevSlideCountFn();
                     }
@@ -170,6 +189,28 @@
 
         },
         section3Fn:function(){
+            var $content = $('#section3 .content');
+            var $contentW = $('#section3 .content').innerWidth();
+            var x = 0;
+            var y = 0;
+
+
+            $content.on({
+                mousemove:function(event){
+                    // 마우스가 위치한곳의 z축값이 높아짐?
+                    // 대상을 사진으로하면 사진전체가 올라와지니까 사진이아닌 다른것?으로 지정해야ㅕ할듯..
+                    // 사진의 중심점을 기준으로 z축값의 변화가있는것같아보임
+                    // console.log('X값',event.clientX);
+                    // console.log('Y값',event.clientY);
+                    x = event.clientX*0.2;
+                    y = event.clientY*0.2;
+                    $content.css({transform:'perspective('+ 2000 +'px) translateZ('+ -y +'px) translateZ('+ -x +'px)'});
+                    // x = event.clientX/($contentW/4);
+                    // y = event.clientY/($contentW/4);
+                    // $content.css({transform:'skewX('+ -x +'deg) skewY('+ y +'deg)'});
+                }
+            });
+
 
         },
         section4Fn:function(){
@@ -292,22 +333,237 @@
             $galleryBtn.each(function(idx){
                 $(this).on({
                     click:function(){
-                        btnNum = idx;
-                        galleryResizeFn();
-                        $galleryBtn.removeClass('addNav');
-                        $(this).addClass('addNav');
+                        if(!$li.is(':animated')){
+                            btnNum = idx;
+                            galleryResizeFn();
+                            $galleryBtn.removeClass('addNav');
+                            $(this).addClass('addNav');
+                        }
                     }
                 });
             });
 
         },
         section5Fn:function(){
+            var $prevBtn = $('#section5 .prev-btn');
+            var $nextBtn = $('#section5 .next-btn');
+            var $stopBtn = $('#section5 .stop-btn');
+            var $playBtn = $('#section5 .play-btn');
+            var $stopPlay = $('#section5 .stop-play');
+            var $ul = $('#section5 ul');
+            var $li = $('#section5 li');
+            var cnt = 0;
+            var n = $('#section5 li').length;
+            var slideW = $('#section5').innerWidth();
+            var angle = 360/n;
+            var tZ = Math.round((slideW/2) / Math.tan(Math.PI/n));
+
+            var setId = null;
+            var setId2 = null;
+            var timercnt = 0;
+
+            var touchS     = 0;
+            var touchE     = 0;
+            var touchD     = false;
+
+
+            function slide3dFn(){
+                $ul.css({transform: 'perspective('+ (tZ*3) +'px) translateZ('+ -tZ +'px) rotateY('+ (-angle*cnt) +'deg)'});
+            }
+            function prevCountFn(){
+                cnt--;
+                slide3dFn();
+            }
+            function nextCountFn(){
+                cnt++;
+                slide3dFn();
+            }
+
+            $prevBtn.on({
+                click:function(){
+                    prevCountFn();
+                    timerFn();
+                    $playBtn.removeClass('addBtn');
+                    $stopBtn.removeClass('addBtn');
+                }
+            });
+            $nextBtn.on({
+                click:function(){
+                    nextCountFn();
+                    timerFn();
+                    $playBtn.removeClass('addBtn');
+                    $stopBtn.removeClass('addBtn');
+                }
+            });
+
+            // $stopPlay.on({
+            //     click:function(){
+            //         $stopBtn.toggleClass('addBtn');
+            //         $playBtn.toggleClass('addBtn');
+            //     }
+            // });
+
+            $stopBtn.on({
+                click:function(){
+                    $stopBtn.addClass('addBtn');
+                    $playBtn.addClass('addBtn');
+                    clearInterval(setId);
+                    clearInterval(setId2);
+                }
+            });
+            $playBtn.on({
+                click:function(){
+                    $playBtn.removeClass('addBtn');
+                    $stopBtn.removeClass('addBtn');
+                    timerFn();
+                }
+            });
+
+            function autoPlayFn(){
+                setId = setInterval(nextCountFn, 5000);
+            }
+            autoPlayFn();
+
+            function timerFn(){
+                timercnt = 0;
+                clearInterval(setId);
+                clearInterval(setId2);
+                setId2 = setInterval(function(){
+                    timercnt++;
+                    if (timercnt >= 5) {
+                        clearInterval(setId2);
+                        nextCountFn();
+                        autoPlayFn();
+                    }
+                }, 1000);
+            }
+
+            $ul.on({
+                mousedown:function(e){
+                    touchD = true;
+                    e.preventDefault();
+                    touchS = e.clientX;
+                },
+                mouseup:function(e){
+                    touchD = false;
+                    e.preventDefault();
+                    touchE = e.clientX;
+                    touchSwipeFn();
+                },
+                mouseleave:function(e){
+                    if(touchD === true){
+                        touchE = e.clientX;
+                        touchSwipeFn();
+                        touchD = false;
+                    }
+                }
+            });
+
+            function touchSwipeFn(){
+                if(touchS-touchE > 30){
+                    nextCountFn();
+                    timerFn();
+                    $playBtn.removeClass('addBtn');
+                    $stopBtn.removeClass('addBtn');
+                }
+                if(touchS-touchE < -30){
+                    prevCountFn();
+                    timerFn();
+                    $playBtn.removeClass('addBtn');
+                    $stopBtn.removeClass('addBtn');
+                }
+            }            
+
 
         },
         section6Fn:function(){
 
         },
         section7Fn:function(){
+            var $slideContainerW = $('#section7 .slide-container').innerWidth();
+            var $slideView       = $('#section7 .slide-view');
+            var $slideWrap       = $('#section7 .slide-wrap');
+            var $slide           = $('#section7 .slide');
+            var $slideW          = $slideContainerW/4;
+            var $prevBtn         = $('#section7 .prev-btn');
+            var $nextBtn         = $('#section7 .next-btn');
+            var imgH             = $('#section7 img').height;
+            var $captionGap      = $('#section7 .caption-gap');
+            var cnt              = 0;
+            var touchS           = 0;
+            var touchE           = 0;
+            var touchD           = false;
+
+
+            function resizeFn(){
+                $slideContainerW = $('#section7 .slide-container').innerWidth();
+                $slideW = $slideContainerW/4;
+                $slideView.css({width:$slideW*4})
+                $slideWrap.css({width:$slideW*15,marginLeft: -$slideW*5});
+                $slide.css({width:$slideW});
+
+                $captionGap.css({height:imgH});
+            }
+
+            $(window).resize(function(){
+                resizeFn();
+            });
+            setTimeout(resizeFn, 100);
+
+
+
+            function slideFn(){
+                $slideWrap.stop().animate({left:-$slideW*cnt}, 400, function(){
+                    if(cnt>4){cnt=0;}
+                    if(cnt<0){cnt=4;}
+                    $slideWrap.stop().animate({left:-$slideW*cnt},0)
+                })
+            }
+            function prevCountFn(){
+                cnt--;
+                slideFn();
+            }
+            function nextCountFn(){
+                cnt++;
+                slideFn();
+            }
+            
+
+
+            $slideView.on({
+                mousedown:function(e){
+                    touchD = true;
+                    e.preventDefault();
+                    touchS = e.clientX;
+                },
+                mouseup:function(e){
+                    touchD = false;
+                    e.preventDefault();
+                    touchE = e.clientX;
+                    touchSwipeFn();
+                },
+                mouseleave:function(e){
+                    if(touchD === true){
+                        touchE = e.clientX;
+                        touchSwipeFn();
+                        touchD = false;
+                    }
+                }
+            });
+
+            function touchSwipeFn(){
+                if(touchS-touchE > 30){
+                    if(!$slideWrap.is(':animated')){
+                        nextCountFn();
+                    }
+                }
+                if(touchS-touchE < -30){
+                    if(!$slideWrap.is(':animated')){
+                        prevCountFn();
+                    }
+                }
+            }            
+
 
         },
         section8Fn:function(){
