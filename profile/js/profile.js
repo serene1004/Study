d8bc8848b8fcc96859aa3c8f5caa09334f5c3944
serene1004 <vcbxnzn@naver.com>
2021-06-02 02:06:02 +0900

    update 2021-06-02

--- profile/js/profile.js
+++ profile/js/profile.js
@@ -3,6 +3,10 @@
     var profile = {
         init:function(){
             this.section1Fn();
+            this.section2Fn();
+            this.section3Fn();
+            this.section4Fn();
+            this.wheelEventFn();
         },
         section1Fn:function(){
             var $winW  = $(window).width();
@@ -14,6 +18,8 @@
             var $a = $('#section1 .link-wrap > span > a');
             var $span = $('#section1 .link-wrap > span');
             var $arrow = $('#section1 .link-wrap > span > i');
+            var $profile = $('#section1 .profile > ul > li');
+            
 
             function resizeFn(){
                 $winW = $(window).width();
@@ -40,21 +46,17 @@
                 $(this).on({
                     mouseenter:function(){
                         if (idx === 0) {
-                            $section1.css({background: 'url(./img/bg1.jpg) no-repeat 50% 50%', backgroundSize: 'cover'});
-                            $a.css({textShadow: '4px 8px 4px rgba(255, 255, 255, 1)'});
-                            $arrow.css({textShadow: '4px 8px 4px rgba(255, 255, 255, 1)'})
+                            $section1.css({background: 'url(./img/main2.jpg) no-repeat 50% 50%', backgroundSize: 'cover'});
+                            $profile.removeClass('addAni');
                         } else if (idx === 1) {
-                            $section1.css({background: 'url(./img/bg2.jpg) no-repeat 50% 50%', backgroundSize: 'cover'});
-                            $a.css({textShadow: '4px 8px 4px rgba(255, 255, 255, 1)'});
-                            $arrow.css({textShadow: '4px 8px 4px rgba(255, 255, 255, 1)'});
+                            $section1.css({background: 'url(./img/main3.jpg) no-repeat 50% 50%', backgroundSize: 'cover'});
+                            $profile.removeClass('addAni');
                         } else if (idx === 2) {
-                            $section1.css({background: 'url(./img/bg0.jpg) no-repeat 50% 50%', backgroundSize: 'cover'});
-                            $a.css({textShadow: '4px 8px 4px rgba(0, 0, 0, .7)'});
-                            $arrow.css({textShadow: '4px 8px 4px rgba(0, 0, 0, .7)'});
+                            $section1.css({background: 'url(./img/main1.jpg) no-repeat 50% 50%', backgroundSize: 'cover'});
+                            $profile.addClass('addAni');
                         } else if (idx === 3) {
-                            $section1.css({background: 'url(./img/bg3.jpg) no-repeat 50% 50%', backgroundSize: 'cover'});
-                            $a.css({textShadow: '4px 8px 4px rgba(0, 0, 0, .7)'});
-                            $arrow.css({textShadow: '4px 8px 4px rgba(0, 0, 0, .7)'});
+                            $section1.css({background: 'url(./img/main4.jpg) no-repeat 50% 50%', backgroundSize: 'cover'});
+                            $profile.removeClass('addAni');
                         }
                     }
                 });
@@ -66,11 +68,189 @@
                     $(this).children(0).css({display:'block'});
                     $(this).children(2).css({display:'block'});
                 }
-            })
+            });
+
 
             
+        },
+        section2Fn:function(){
+            var $winW  = $(window).width();
+            var $winH  = $(window).height();
+            var $section2 = $('#section2');
+            var $p = $('#section2 .title > p');
+            var $h2 = $('#section2 .title > h2');
+            var $ulH3 = $('#section2 .title > ul > h3');
+            var $li = $('#section2 .title > ul > li');
+            var $btnWrap = $('#section2 .title .btn-wrap');
+            var t = 0;
+
+            function resizeFn(){
+                $winW = $(window).width();
+                $winH  = $(window).height();
+
+                $section2.css({width:$winW,height:$winH})
+            }
+
+            $(window).resize(function(){
+                resizeFn();
+            });
+            setTimeout(resizeFn, 100);
+
+            $(window).scroll(function(){
+                if($(window).scrollTop() >= $('#section2').offset().top){
+                    if(t === 0){
+                        t = 1;
+                        $btnWrap.addClass('addScroll');
+                        $p.addClass('addScroll');
+                        $h2.addClass('addScroll');
+                        $ulH3.addClass('addScroll');
+                        $li.addClass('addScroll');
+                    }
+                }
+                if($(window).scrollTop() === $('#section1').offset().top){
+                    t = 0;
+                    $btnWrap.removeClass('addScroll');
+                    $p.removeClass('addScroll');
+                    $h2.removeClass('addScroll');
+                    $ulH3.removeClass('addScroll');
+                    $li.removeClass('addScroll');
+                }
+            });
+
+        },
+        section3Fn:function(){
+            var $winW  = $(window).width();
+            var $winH  = $(window).height();
+            var $section3 = $('#section3');
+            var $p = $('#section3 .title > p');
+            var $h2 = $('#section3 .title > h2');
+            var $ulH3 = $('#section3 .title > ul > h3');
+            var $li = $('#section3 .title > ul > li');
+            var $btnWrap = $('#section3 .title .btn-wrap');
+            var t = 0;
+
+            function resizeFn(){
+                $winW = $(window).width();
+                $winH  = $(window).height();
+
+                $section3.css({width:$winW,height:$winH})
+            }
+
+            $(window).resize(function(){
+                resizeFn();
+            });
+            setTimeout(resizeFn, 100);
+
+            $(window).scroll(function(){
+                if($(window).scrollTop() >= $('#section3').offset().top){
+                    if(t === 0){
+                        t = 1;
+                        $btnWrap.addClass('addScroll');
+                        $p.addClass('addScroll');
+                        $h2.addClass('addScroll');
+                        $ulH3.addClass('addScroll');
+                        $li.addClass('addScroll');
+                    }
+                }
+                if($(window).scrollTop() === $('#section2').offset().top){
+                    t = 0;
+                    $btnWrap.removeClass('addScroll');
+                    $p.removeClass('addScroll');
+                    $h2.removeClass('addScroll');
+                    $ulH3.removeClass('addScroll');
+                    $li.removeClass('addScroll');
+                }
+            });
+
+        },
+        section4Fn:function(){
+            var $winW  = $(window).width();
+            var $winH  = $(window).height();
+            var $section4 = $('#section4');
+            var $p = $('#section4 .title > p');
+            var $h2 = $('#section4 .title > h2');
+            var $ulH3 = $('#section4 .title > ul > h3');
+            var $li = $('#section4 .title > ul > li');
+            var $btnWrap = $('#section4 .title .btn-wrap');
+            var t = 0;
+
+            function resizeFn(){
+                $winW = $(window).width();
+                $winH  = $(window).height();
+
+                $section4.css({width:$winW,height:$winH})
+            }
+
+            $(window).resize(function(){
+                resizeFn();
+            });
+            setTimeout(resizeFn, 100);
+
+            $(window).scroll(function(){
+                if($(window).scrollTop() >= $('#section4').offset().top){
+                    if(t === 0){
+                        t = 1;
+                        $btnWrap.addClass('addScroll');
+                        $p.addClass('addScroll');
+                        $h2.addClass('addScroll');
+                        $ulH3.addClass('addScroll');
+                        $li.addClass('addScroll');
+                    }
+                }
+                if($(window).scrollTop() === $('#section3').offset().top){
+                    t = 0;
+                    $btnWrap.removeClass('addScroll');
+                    $p.removeClass('addScroll');
+                    $h2.removeClass('addScroll');
+                    $ulH3.removeClass('addScroll');
+                    $li.removeClass('addScroll');
+                }
+            });
+
+        },
+        wheelEventFn:function(){
+            var $main = $('#main');
+            var $section = $('.section');
+            var wheelDelta = 0;
+            var cnt = 0;
+
+            var $goTopBtn = $('.gotop-btn');
+
+            $goTopBtn.on({
+                click:function(event){
+                    event.preventDefault();
+                    cnt=0;
+                    var url = $(this).attr('href');
+                    $('html,body').stop().animate({scrollTop:$(url).offset().top}, 800);
+                }
+            });
 
 
+            $main.on('mousewheel DOMMouseScroll',function(event){
+                event.preventDefault();
+
+                if (event.originalEvent.wheelDelta) {
+                    wheelDelta = event.originalEvent.wheelDelta;
+                } else {            // 파이어폭스
+                    wheelDelta = event.detail*-1
+                }
+
+                if (!$('html,body').is(':animated')) {
+                    if(wheelDelta < 0) {
+                        cnt++;
+                        if (cnt>=3) {
+                            cnt=3;
+                            $('html,body').stop().animate({scrollTop:$section.eq(cnt).offset().top}, 800, 'swing');
+                        } else {
+                            $('html,body').stop().animate({scrollTop:$section.eq(cnt).offset().top}, 800, 'swing');
+                        }
+                    } else {
+                        cnt--;
+                        if (cnt<0) {cnt=0;}
+                        $('html,body').stop().animate({scrollTop:$section.eq(cnt).offset().top}, 800, 'swing');
+                    }
+                }
+            });
         }
         
     }
