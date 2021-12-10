5d7f7410142efd2b25151424aeb63b5021ce1ba9
serene1004 <vcbxnzn@naver.com>
2021-12-10 11:22:58 +0900

    update 2021-12-10

---
+++ SCSS/js/style.js
@@ -0,0 +1,14 @@
+(function ($) {
+
+    let style = {
+        init: function () {
+            this.style01Fn();
+        },
+        style01Fn: function () {
+
+        }
+    }
+
+    style.init();
+
+})(jQuery);
