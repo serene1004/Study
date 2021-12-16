cef7a6aa04e0be551c487c6203422289f27a9fe3
serene1004 <vcbxnzn@naver.com>
2021-12-16 21:04:33 +0900

    update 2021-12-16

--- SCSS/js/style.js
+++ SCSS/js/style.js
@@ -2,10 +2,29 @@
 
     let style = {
         init: function () {
-            this.style01Fn();
+            this.section02Fn ();
         },
-        style01Fn: function () {
+        section02Fn: function () {
+            
+            function shootingStar (){
+                let amount = 30;
+                let section02 = document.querySelector('#section02');
+                let i = 0;
 
+                while(i < amount){
+                    let drop = document.createElement('i');
+
+                    let size = Math.random() * 5;
+                    let positionX = Math.floor(Math.random() * window.innerWidth);
+                    let aniDelay = Math.random() * -50 ;
+                    drop.style.width = 0.1 + size + 'px';
+                    drop.style.left = positionX + 'px';
+                    drop.style.animationDelay = aniDelay + 's';
+                    section02.appendChild(drop);
+                    i++;
+                }
+            }
+            shootingStar ();
         }
     }
 
