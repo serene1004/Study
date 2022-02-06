ecf9250d899dd8709f72704474be9bc6320371f4
serene1004 <vcbxnzn@naver.com>
2022-02-07 01:05:17 +0900

    2022-02-07

--- Project/js/style.js
+++ Project/js/style.js
@@ -1,14 +1,14 @@
 function introAnimation () {
-    let intro = document.querySelector('.intro-bg');
-    let introBar = document.querySelectorAll('.intro-bg div');
+    let intro = document.querySelector('.intro_bg');
+    let introBar = document.querySelectorAll('.intro_bg div');
     let introBarCnt = introBar.length;
 
     intro.onclick = function(){
         for (let i = 0; i < introBarCnt; i ++) {
             if (i % 2 === 0) {
-                introBar[i].style.transform = 'skew(20deg) translateX('+ (20+(10*i)) + 'vw) translateY(-100vh)';
+                introBar[i].style.transform = 'skew(30deg) translateX('+ (20+(10*i)) + 'vw) translateY(-100vh)';
             } else if (i % 2 === 1) {
-                introBar[i].style.transform = 'skew(20deg) translateX('+ (20+(10*i)) + 'vw) translateY(100vh)';
+                introBar[i].style.transform = 'skew(30deg) translateX('+ (20+(10*i)) + 'vw) translateY(100vh)';
             }
         }
 
@@ -18,21 +18,50 @@ function introAnimation () {
             setTimeout(function(){
                 intro.style.display = 'none';
             }, 1000)
+            backgroundVideo();
         }, 1500)
     }
 }
 introAnimation();
 
+function backgroundVideo () {
+    let video = document.querySelector('.bg_video video');
+    let videoProgress = document.querySelector('.progress_bar span');
 
-// 제이쿼리 안쓰고 해보자
-(function ($) {
-    let style = {
-        init: function () {
-            this.styleFn();
-        },
-        styleFn: function () {
+    video.play();
+    video.style.transform = 'scale(1)'
+    videoProgress.classList.add('on')
+}
 
-        }
-    }
-    style.init();
-})(jQuery);
+function svgCircleAnimation () {
+    // let svgBtn = document.querySelectorAll('.btn_wrap');
+    // let svgBtnCnt = svgBtn.length;
+    // let svgCircle = document.querySelectorAll('.btn_wrap circle');
+    // let svgCircleCnt = svgCircle.length;
+
+    // for (let i = 0; i < svgBtnCnt; i++) {
+    //     svgBtn[i].addEventListener('mouseleave', function(){
+    //         for (let j = 0; j < svgCircleCnt; j++) {
+    //             svgCircle[j].classList.add('on');
+    //             setTimeout(function() {
+    //                 svgCircle[j].classList.remove('on');
+    //             }, 300)
+    //         }
+    //     });
+    // }
+
+    document.querySelectorAll('.btn_wrap').forEach(function (el) {
+        let circle = el.querySelector('circle')
+
+        el.addEventListener('mouseleave', function(){
+            circle.classList.add('on');
+            setTimeout(function() {
+                circle.classList.remove('on');
+            }, 300)
+        })
+    })
+}
+svgCircleAnimation ();
+
+
+// 현재 보여지고있는 화면에서 그라디언트가 움직이고있음. 이건어케하는거지?
